9b38979f82b7dbbd6aa6b89010c1791c9305c4b2
dlachdla <chyolim4692@naver.com>
2023-12-03 22:28:14 +0900

    12/03 수정13

--- js/index.js
+++ js/index.js
@@ -1,34 +1,28 @@
-const check_id = document.querySelector("#username");
-const check_pwd = document.querySelector("#password");
 const check_login = document.querySelector("#login_bin");
-const hidden = document.querySelector(".wrap");
-const visible = document.querySelector("#menu");
+const login = document.querySelector(".wrap");
+const category = document.querySelector("#category");
 
-function checkValue(){
-    const ID = check_id.value;
-    const PassWord = check_pwd.value;
+const checkValue = () => {
+    const loginUsername = document.querySelector('#username').value;
+    const loginPassword = document.querySelector('#password').value;
 
-    // console.log(ID, PassWord);
-   // localStorage 에서 members 읽어오기
-    const members = JSON.parse(localStorage.getItem('members'));
+   // members로 저장된 배열이 있다면 그걸사용하고, 없으면 새 배열을 생성한다
+    const members = JSON.parse(localStorage.getItem('members')) || [];
 
-    members.forEach((member) => {
-        const {id, password} = member;
-        // console.log(id, password);
-        if(id == ID && password == PassWord){
-            alert(`❤ ${id}회원님 환영합니다 ❤`);
-            hidden.style.visibility = 'hidden';
-            visible.style.visibility = 'visible';
-            return true;
-            // window.location.href = 'members.html'
-        }else{
-            alert("💦 ID와 Password를 다시 입력해주세요. 💦");
-            return false;
-        }
-    });
+    //  아이디와 비밀번호가 일치하는 회원 찾기
+    const memberInfo = members.find(member => member.id === loginUsername && member.password === loginPassword);
+    
+    if (memberInfo) {
+        alert(`❤ ${memberInfo.id}회원님 환영합니다 ❤`);
+        login.style.visibility = 'hidden';
+        category.style.visibility = 'visible';
+    } else {ej
+        alert("💦 ID와 Password를 다시 입력해주세요. 💦");
+    }
 };
-
 function init(){
     check_login.addEventListener("click", checkValue);
 }
 init();
+
+
